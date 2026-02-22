0cecfd0c1bb2a7d370e39de1ff385e4f97c83621
vovados1 <doskovol1@gmail.com>
2026-02-22 00:01:45 -0600

    Disable cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono"
 import { websocket } from "hono/bun"
+import { cors } from "hono/cors"
 import { dataRoute } from "./routes/Data"
 
 const HOSTNAME = String(process.env.HOSTNAME) || "localhost"
 const PORT = Number(process.env.PORT) || 7000
 
 const app = new Hono()
+
+// Defining middleware
+app.use(cors())
 
 // Defining routes
 app.route("/data", dataRoute)
